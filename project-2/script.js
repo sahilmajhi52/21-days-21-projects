946372245c47f4758c9ca7b903692c1c00d5f0c0
sahil Majhi <sahilmajhi52@gmail.com>
2026-01-18 10:52:08 +0530

    project-2

---
+++ project-2/script.js
@@ -0,0 +1,49 @@
+//TODO: please add a footer in index.html file before making the submission
+
+const toggleBtn = document.getElementById("themeToggle");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light-theme");
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("light-theme") ? "light" : "dark"
+  );
+  const isLight = document.body.classList.contains("light-theme");
+  toggleBtn.textContent = isLight ? "🌙" : "🌞";
+});
+
+const typedTextSpan = document.querySelector(".typed-text");
+const textArray = ["Software Engineer", "Mentor", "Comedian", "Runner"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (textArray.length) setTimeout(type, newTextDelay);
+});
