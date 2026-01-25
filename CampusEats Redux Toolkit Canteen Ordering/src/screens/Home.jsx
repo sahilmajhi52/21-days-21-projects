7ca5a53e0b664e2f8423112cd0181792bf8a4cff
sahil Majhi <sahilmajhi52@gmail.com>
2026-01-25 20:55:08 +0530

    setup for canteen website

---
+++ CampusEats Redux Toolkit Canteen Ordering/src/screens/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import RestaurantGrid from "../Components/RestaurantGrid";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <RestaurantGrid />
+    </div>
+  );
+};
+
+export default Home;
